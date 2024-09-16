dcfff30557c03d630e8559bbedc48b0d868e8a86
kevin <kevinhamelink05@gmail.com>
2024-09-16 12:19:13 +0200

    School if else

--- Console.js
+++ Console.js
@@ -1,18 +1,17 @@
 const name = "Kevin";
 const age = 10;
 const docent = "Draggan";
-
+console.log("-----------------------------------------------------------");
 console.log("Mijn naam is " + name);
 console.warn("Warn " + age);
 console.warn("docent naam " + docent)
-
-const date2 = new Date(2024, 7, 12, 10, 39, 5, 10)
-
+console.log("-----------------------------------------------------------");
+new Date(2024, 7, 12, 10, 39, 5, 10);
 console.log(new Date(1985, 6, 12)); // 12 juli 1985
 console.log(new Date(1955, 8, 23)); // 23 september 1955
 console.log(new Date(2015, 7, 31)); // 31 augustus 2015
 console.log(new Date(1885, 0, 4)); // 4 januari 1885
-
+console.log("-----------------------------------------------------------");
 
 let player = {
     name: "Erwin Henraat",
@@ -32,3 +31,17 @@ for (let key in player) {
     console.log(`${key}: ${player[key]}`);
 }
 
+console.log("-----------------------------------------------------------");
+
+
+let huidigeTijd = new Date();
+console.log(huidigeTijd.getHours() + ":" + huidigeTijd.getMinutes() + ":" + huidigeTijd.getSeconds());
+
+if (huidigeTijd.getHours() > 9) {
+    console.log("Op school");
+} else if(huidigeTijd.getHours() < 9) {
+    console.log("Niet op school");
+} else if(huidigeTijd.getHours() > 16) {
+    console.log("School is klaar");
+}
+console.log("-----------------------------------------------------------");
