f392a1000844c09beef55327c9db7f63cebae33d
kevin <kevinhamelink05@gmail.com>
2024-09-16 11:30:04 +0200

    Console 'game' made

--- Console.js
+++ Console.js
@@ -5,3 +5,30 @@ const docent = "Draggan";
 console.log("Mijn naam is " + name);
 console.warn("Warn " + age);
 console.warn("docent naam " + docent)
+
+const date2 = new Date(2024, 7, 12, 10, 39, 5, 10)
+
+console.log(new Date(1985, 6, 12)); // 12 juli 1985
+console.log(new Date(1955, 8, 23)); // 23 september 1955
+console.log(new Date(2015, 7, 31)); // 31 augustus 2015
+console.log(new Date(1885, 0, 4)); // 4 januari 1885
+
+
+let player = {
+    name: "Erwin Henraat",
+    jumpforce: 20,
+    specialPower: "teleport",
+    speed: 280,
+    bike: "Honda VFR VTEC",
+    livesLeft: 1,
+    ammo: 121,
+    numberOfLasers: 5,
+    batteryPower: 90,
+    level: 9000,
+    wanted: true
+};
+
+for (let key in player) {
+    console.log(`${key}: ${player[key]}`);
+}
+
